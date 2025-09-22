9e1ac82b846d73da65beebaebbd08deb6dc83b03
Ahmedaligue <158603161+Ahmedaligue@users.noreply.github.com>
2025-09-22 13:02:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const config = {
   prefixes: ["/", ".", "#", "!"],
-  owner: '5493772455367',
-  botName: 'ZenBot',
-  ownerName: 'AxelDev',
-  instagram: 'https://www.instagram.com/_axel.vega',
-  github: 'https://github.com/AxelDev09/ZenBot',
-  atom: 'https://www.atom.bio/zenbot',
+  owner: '212625457341',
+  botName: '『ZHAWFUN BOT』•ᴮᴬᴰʙᴏʏツ',
+  ownerName: '🍿🎥✮⋆˙ AHMED ALIGUE °🥂⋆.ೃ🍾࿔*:･',
+  instagram: 'https://www.instagram.com/g_boy072',
+  github: 'https://github.com/Ahmedaligue/ZenBot',
+  atom: 'https://www.atom.bio/ahmedaligue',
 };
